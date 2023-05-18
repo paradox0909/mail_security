a9b5c26db88dd79114a53a841fefbeb2b60b97c9
Paradox123 <dokingkns0909@naver.com>
2023-05-18 10:46:35 +0000

    succes

---
+++ send_mail/maintest.py
@@ -0,0 +1,28 @@
+#have to on the sendmail
+#stmp mail sender 
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+with open('mail_body.txt', 'r') as f:
+    body = f.read()
+
+
+sender = 'dokingkns2006@gmail.com'
+recipients = open('receivers.txt', 'r')
+recipient= recipients.readline()
+
+while recipient :   
+    changebody = body.replace('tempmail',recipient)
+    msg = MIMEText(changebody,"html")
+    msg['Subject'] = 'title'
+    msg['From'] = sender
+    msg['To'] = recipient
+
+    try:
+        smtpObj = smtplib.SMTP('localhost')
+        smtpObj.sendmail(sender, recipient, msg.as_string())         
+        print("Email sent successfully.") 
+        recipient= recipients.readline()
+    except Exception as e:
+        print("error has occurred.", e)
